c8961a6cdd526df9c5601cc32a45ba3a7850f9b1
Jaideep Singh <32881591+jaideep2@users.noreply.github.com>
2023-10-02 16:37:11 -0700

    fixed moved.py

--- moved.py
+++ moved.py
@@ -1,8 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="Topic2Clip", page_icon="🍿", layout="centered", initial_sidebar_state="collapsed")
-# Set header
-st.markdown(f"<h1 style='text-align: center;'>Topic<font color='#ff0000'>2</font>Video🍿Generator</h2><br>", unsafe_allow_html=True)
-st.markdown(f"""<h4 style='text-align: center; font-style: italic;'>Thanks for using the service. The new generation service is hosted here: </h4><br>""", unsafe_allow_html=True)
-st.link_button("Topic2Video Generator", "https://topic2video.streamlit.app", use_container_width=True)
-st.image(f"""<h4 style='text-align: center; font-style: italic;'>If you recieved a link to view a video here, you'll need to regenerate them through above link.</h4><br>""")
+st.subheader("Thanks for using the service. The new service is hosted here")
+st.link_button("Topic2Video Generator", "https://topic2video.streamlit.app", type="secondary")
+st.caption("If you recieved a link to view a video here, you'll need to regenerate them through above link.")
 st.stop()
